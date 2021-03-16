314680181d046b7cd017b30964cdd76c3f756157
BowiePro <bowieproyt@gmail.com>
2021-03-16 16:24:56 -0400

    Add spritesheet

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,42 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float movementSpeed;
     public Rigidbody2D rb;
 
-    public float jumpForce = 20f;
+
+    public Animator anim;
+
+    public float JumpForce = 20f;
     public Transform feet;
-    public LayerMask groundLayers; 
+    public LayerMask groundLayers;
 
     float mx;
 
-    private void Update() {
+    private void Update()
+    {
         mx = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
             Jump();
         }
+
+        if (Mathf.Abs(mx) > 0.05f)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (mx > 0f)
+        {
+            transform.localScale = new Vector3(2f, 2f, 2f);
+        }
+        else if (mx < 0f)
+        {
+            transform.localScale = new Vector3(-2f, 2f, 2f);
+        }
+
+        anim.SetBool("IsGrounded", isGrounded());
     }
-        private void FixedUpdate() {
-            Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
 
-            rb.velocity = movement;
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, JumpForce);
+
+        rb.velocity = movement;
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
         }
-        void Jump() {
-            Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
 
-            rb.velocity = movement;
-        }
-
-        public bool IsGrounded() {
-            Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
-
-            if (groundCheck != null)
-            {
-                return true;
-            }
-
-            return false;
-        }
+        return false;
+    }
 }
